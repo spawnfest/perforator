bff46c1df80a0f6af77f06395c9efc2c450d43ba
Ignas Vyšniauskas <baliulia@gmail.com>
2012-07-07 17:35:06 +0300

    Update types.

--- src/perforator_types.erl
+++ src/perforator_types.erl
@@ -1,12 +1,27 @@
 %% @doc Perforator types
 -module(perforator_types).
+
 -export_type([
-    test_fun_desc/0,
-    test_fun_type/0,
-    test_fun/0
+    test_fun/0,
+    test_obj/0,
+    primitive_test_obj/0,
+    fixture_test_obj/0
 ]).
 
-
--type test_fun_desc() :: {test_fun_type(), test_fun()}.
--type test_fun_type() :: raw | generator.
--type test_fun() :: fun(() -> term()).
+-type test_obj() :: primitive_test_obj() | fixture_test_obj().
+-type fixture_test_obj() :: setup_fixture() | foreach_fixture().
+-type setup_fixture() ::
+    {setup, Setup :: fun(() -> any()), Cleanup :: fun((any()) -> any()),
+        test_obj()}.
+-type foreach_fixture() ::
+    {foreach, Setup :: fun(() -> any()), Cleanup :: fun((any()) -> any()),
+        [test_obj()]}.
+%% @todo Fix!
+-type primitive_test_obj() :: test_fun() | test_fun_decorator() | fun().
+-type test_fun_decorator() :: repeat_decorator() | desc_decorator().
+-type repeat_decorator() ::
+    {repeat, test_fun(), Times :: pos_integer(), Sleep :: timer:time()}.
+-type desc_decorator() ::
+    {desc, string(), test_fun()}.
+-type test_fun() :: {raw_fun, {Module::atom(), Funtion::atom(),
+    Arity::non_neg_integer()}}.
