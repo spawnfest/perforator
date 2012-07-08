22656e57a085bec756e237480938f0a12d82d64e
Ignas Vyšniauskas <baliulia@gmail.com>
2012-07-08 11:52:32 +0300

    Add test for correct duration gathering. Add ?assertApprox(..).

--- include/eunit_utils.hrl
+++ include/eunit_utils.hrl
@@ -1,7 +1,11 @@
-%% Checks whether Sublist is a sublist of List, order does not matter.  Can
+%% ============================================================================
+%% ?assertSublist(Sublist, List)
+%%
+%% @doc Checks whether Sublist is a sublist of List, order does not matter.  Can
 %% also be used to check whether a proplist is a sublist. Usage of wild cards
 %% in {Key, Value} tuples allowed, i.e.  ?assertSublist([{k, '_'}], [{k, 2}])
 %% will return 'ok'. Repeated elements matter (we are not checking sets).
+%% ============================================================================
 
 -ifdef(NOASSERT).
 -define(assertSublist(Sublist, List), ok).
@@ -55,4 +59,35 @@
 ).
 -endif.
 -define(_assertSublist(Sublist, List), ?_test(?assertSublist(Sublist, List))).
+
+%% ============================================================================
+%% ?assertApprox(ExpectedValue, ObservedValue)
+%%
+%% @doc Tests for no statistically significant difference between the numeric
+%% values.
+%% ============================================================================
+
+-define(CONFIDENCE_VALUE, 0.98).
+
+-ifdef(NOASSERT).
+-define(assertApprox(ExpectedValue, ObservedValue), ok).
+-else.
+-define(assertApprox(ExpectedValue, ObservedValue),
+    ((fun () ->
+        case (abs(ObservedValue - ExpectedValue) / ExpectedValue) of
+           X when X =< (1 - ?CONFIDENCE_VALUE) ->
+                ok;
+            _ ->
+                erlang:error({assertApprox_failed, [
+                    {module, ?MODULE},
+                    {line, ?LINE},
+                    {expected_value, ExpectedValue},
+                    {observed_value, ObservedValue}
+                ]})
+        end
+    end)())
+).
+-endif.
+-define(_assertApprox(ExpectedValue, ObservedValue),
+    ?_test(?assertSublist(ExpectedValue, ObservedValue))).
 
--- test/perforator_tests.erl
+++ test/perforator_tests.erl
@@ -5,6 +5,8 @@
 -include("include/eunit_utils.hrl").
 -include("include/log_utils.hrl").
 
+-define(TEST_FUN_SLEEP, 100). % 100ms
+
 -compile(export_all).
 
 perforator_test_() ->
@@ -12,6 +14,7 @@ perforator_test_() ->
         fun setup/0,
         fun cleanup/1,
         [
+            {"Correct duration", fun test_correct_duration/0},
             {"{foreach, ..} fixture test", fun test_foreach_perf/0}
         ]
     }.
@@ -27,12 +30,21 @@ cleanup(_) ->
     ?silent(perforator:stop_deps()),
     ok.
 
+test_correct_duration() ->
+    Test = {raw_fun, {?MODULE, named_fun_perf, 0}},
+    Result = perforator:run_test(Test),
+    ?assertMatch({named_fun_perf, _}, Result),
+    lists:foreach(fun (Run) ->
+        Duration = get_duration(Run),
+        ?assertApprox(?TEST_FUN_SLEEP*1000, Duration)
+    end, get_runs(Result)).
+
 test_foreach_perf() ->
-    Test = {foreach, fun () -> ok end, fun (_) -> ok end, [
-        fun () -> ok end,
-        fun (_) -> ok end
+    TestObj = {foreach, fun () -> ok end, fun (_) -> ok end, [
+        fun () -> ok end, %% test case 1
+        fun (_) -> ok end %% test case 2
     ]},
-    Results = perforator:run_test(Test),
+    Results = perforator:run_test(TestObj),
     ?assertEqual(2, length(Results)),
     Result1 = hd(Results),
     Result2 = hd(tl(Results)),
@@ -54,9 +66,19 @@ test_foreach_perf() ->
     CheckEverythingFun(Runs2).
 
 %% ============================================================================
+%% Perf test functions
+%% ============================================================================
+
+named_fun_perf() ->
+    timer:sleep(?TEST_FUN_SLEEP).
+
+
+%% ============================================================================
 %% Helpers
 %% ============================================================================
 
 get_runs({_Name, Results}) ->
-    proplists:get_value(runs, Results, []).
+    proplists:get_value(runs, Results).
 
+get_duration({_Name, Results}) ->
+    proplists:get_value(duration, Results).
